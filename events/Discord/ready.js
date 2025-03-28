e553500c65d40d4c59fa277f7e595137af328540
titiktemuroleplay <rtsanhok18@gmail.com>
2025-03-28 12:00:46 +0700

    a

--- events/Discord/ready.js
+++
@@ -1,8 +0,0 @@
-const { Translate } = require('../../process_tools');
-
-module.exports = async (client) => {
-    console.log(await Translate(`Logged to the client <${client.user.username}>.`));
-    console.log(await Translate("Let's play some music !"));
-    
-    client.user.setActivity(client.config.app.playing);
-}
--- events/Player/playerFinish.js
+++
@@ -1,8 +0,0 @@
-module.exports = (queue) => {
-    if (queue.metadata.lyricsThread) {
-        queue.metadata.lyricsThread.delete();
-        queue.setMetadata({
-            channel: queue.metadata.channel
-        });
-    }
-}
